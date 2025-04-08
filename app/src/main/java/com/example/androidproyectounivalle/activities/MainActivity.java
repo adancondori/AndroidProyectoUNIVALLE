e72f57d3843c19ca34e9deda5eeab5667b0afd7d
eidan <adancondoric@gmail.com>
2025-04-08 09:21:56 -0400

    return value screem

--- app/src/main/java/com/example/androidproyectounivalle/activities/MainActivity.java
+++ app/src/main/java/com/example/androidproyectounivalle/activities/MainActivity.java
@@ -29,6 +29,37 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         init();
+        System.out.println("Estoy en el onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("Estoy en el onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Estoy en el onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Estoy en el onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("Estoy en el onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("Estoy en el onRestart");
     }
 
     public void init(){
